5d25bd45b98646159701212433427b6d7b4bf2be
AJIT SHARMA <ajit08082000@gmail.com>
2021-09-29 17:34:08 +0530

    improved input handling method and added comments

--- app.js
+++ app.js
@@ -3,35 +3,41 @@ var stockQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
-
+// initialize the function for checking its input methods as per requirements
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+    if (!ip || !qty || !curr) {
+        outputBox.innerText = "Fill all the fields";
+    } else if (ip <= 0 || qty <= 0 || curr <= 0) {
+        outputBox.innerText = "Please Fill the positive value";
+    } else {
+        calculateProfitAndLoss(ip, qty, curr);
+    }
+}
 submitBtn.addEventListener("click", submitHandler);
 
+function calculateProfitAndLoss(initialRs, quantity, currentRs) {
+    // creating a condition for loss and loss and loss percentage
+    if (initialRs > currentRs) {
+        var loss = ((initialRs - currentRs) * quantity).toFixed(2);
+        var lossPercentage = ((loss * 100) / (initialRs * quantity));
+        lossPercentage = lossPercentage.toFixed(2);
 
-function submitHandler() {
-    var ip = initialPrice.value;
-    var qty = stockQuantity.value;
-    var curr = currentPrice.value;
+        showOutput(`hey the loss is ${loss} and percent is ${lossPercentage}%`)
 
-    calculateProfitAndLoss(ip, qty, curr);
-
-}
-
-function calculateProfitAndLoss(initial, quantity, current) {
-    if (initial > current) {
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;
-        showOutput(`hey the loss is ${loss} and percent is ${lossPercentage}%`);
-    } else if (current > initial) {
-        var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100;
-
-        showOutput(`hey the profit is ${profit} and percent is ${profitPercentage}%`)
+        // creating a condition for profit and profit percentage
+    } else if (initialRs < currentRs) {
+        var profit = ((currentRs - initialRs) * quantity).toFixed(2);
+        var profitPercentage = ((profit * 100) / (initialRs * quantity));
+        profitPercentage = profitPercentage.toFixed(2);
+        showOutput(`hey the profit is ${profit} and percent is ${profitPercentage} %`);
     } else {
         showOutput("Don't be lazy, enter something !!")
     }
 }
-
-
-function showOutput(message) {
-    outputBox.innerHTML = message;
+//initializing msg function
+function showOutput(msg) {
+    outputBox.innerHTML = msg;
 }
